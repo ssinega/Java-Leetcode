1212dece732e015ba81b24ee578a2284e65b895f
Sinega Selvakumar <sinegas1652@gmail.com>
2025-07-23 19:52:48 +0530

    Update two sum.java

--- two sum.java
+++ two sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer,Integer> map=new HashMap<>();
+        HashMap <Integer,Integer> map=new HashMap<>(); //hashmap - store numbers and their indices
         for(int i=0;i<nums.length;i++)  {
             int complement = target - nums[i];
-            if (map.containsKey(complement))  {
-                return new int[] {map.get(complement),i};
+            if (map.containsKey(complement))  { //check complement already exists in map
+                return new int[] {map.get(complement),i}; //return the indices
 
             }
             map.put(nums[i],i);
